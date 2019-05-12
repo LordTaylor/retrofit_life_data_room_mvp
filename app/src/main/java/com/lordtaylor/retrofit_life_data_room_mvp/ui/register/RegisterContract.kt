19af2461ca205599493544e2f61609fa3b737f99
LordTaylor <krawczyk.jaroslaw84@gmail.com>
2019-05-12 11:27:50 +0200

    all contracts added

--- app/src/main/java/com/lordtaylor/retrofit_life_data_room_mvp/ui/register/RegisterContract.kt
+++ app/src/main/java/com/lordtaylor/retrofit_life_data_room_mvp/ui/register/RegisterContract.kt
@@ -1,8 +1,8 @@
-package com.lordtaylor.retrofit_life_data_room_mvp.ui.lists
+package com.lordtaylor.retrofit_life_data_room_mvp.ui.register
 
 import com.lordtaylor.retrofit_life_data_room_mvp.ui.base.BaseContract
 
-interface ListBaseContract :BaseContract{
+interface RegisterContract:BaseContract {
     interface Presenter:BaseContract.Presenter<View>
     interface View:BaseContract.View
 }
